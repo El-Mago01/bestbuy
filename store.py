00afa89f53d71db0b1dd5852f96300c7154dc196
El-Mago01 <Martin.Gorren@gmail.com>
2026-06-10 20:27:26 +0200

    Initial implementation of the store.

---
+++ store.py
@@ -0,0 +1,70 @@
+import products
+
+class NotInStore(Exception):
+    pass
+
+
+class Store:
+
+    def __init__(self, product_list, name):
+        self.name = name
+        self.product_list = product_list
+
+
+    def get_store_name(self):
+        """ returns the name of the store"""
+        return self.name
+
+    def add_product(self, product):
+        """add a product to the product list"""
+        self.product_list.append(product)
+
+    def remove_product(self, product):
+        """Removes a product from store."""
+        self.product_list.remove(product)
+
+    def get_total_quantity(self) -> int:
+        """Returns how many items are in the store in total."""
+        total_items = 0
+        for product in self.product_list:
+            total_items += product.quantity
+        return total_items
+
+    def get_all_products(self) -> list:
+        """Returns all products in the store that are active."""
+        active_products = []
+        for product in self.product_list:
+            if product.active:
+                active_products.append(product)
+        return active_products
+
+    def order(self, shopping_list) -> float:
+        """Gets a list of tuples, where each tuple has 2 items:
+        Product (Product class) and quantity (int).
+        Buys the products and returns the total price of the order."""
+        sales = 0
+        for product_list_item, quantity in shopping_list:
+            try:
+                sales += product_list_item.buy(quantity)
+            except products.ProductUnavailable as e:
+                raise NotInStore ("Product not available. ", e)
+        return sales
+
+def main():
+    product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
+                    products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                    products.Product("Google Pixel 7", price=500, quantity=250),
+                    ]
+
+    best_buy = Store(product_list, "BEST BUY")
+    all_products = best_buy.get_all_products()
+    for product in all_products:
+        product.show()
+    print(best_buy.get_total_quantity())
+    print(best_buy.order([(all_products[0], 1), (all_products[1], 2)]))
+    all_products = best_buy.get_all_products()
+    for product in all_products:
+        product.show()
+        
+if __name__ == "__main__":
+    main()
