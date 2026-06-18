2c1ebdc1cc920957baf9ef0f6fe102cb2d9751e9
El-Mago01 <Martin.Gorren@gmail.com>
2026-06-18 18:41:50 +0200

    Best Buy now unit tested with 11 test cases passing

--- products.py
+++ products.py
@@ -12,66 +12,93 @@ class Product:
     """
 
     @staticmethod
-    def calc_price(price, quantity) -> float:
+    def calc_price(price:float, quantity:int) -> float:
         """
         Just fooling around with static methods
         """
         return price * quantity
 
-    def __init__(self, name, price, quantity):
+    def __init__(self, name, price, quantity) -> None:
         if len(name) == 0:
             raise ValueError("Product name can not be empty")
-        self.name = name
-        try:
-            self.price = price
-            self.quantity = quantity
-        except ValueError as e:
-            raise ValueError(
-                "Please ensure price is a value in $ and quantity is a number"
-            ) from e
-        self.active = True
+        self.__name = name
+        if not (isinstance(price, float) or isinstance(price, int)):
+            raise ValueError("Please ensure price is valid")
+        if price < 0:
+            raise ValueError("Please ensure price is valid")
+        self.__price = price
+        if not isinstance(quantity, int):
+            raise ValueError("Please ensure quantity is a valid number")
+        if quantity < 0:
+            raise ValueError("Please ensure quantity is a valid number")
+        self.__quantity:int = quantity
+        self.__active = True
 
     def get_quantity(self) -> int:
         """Getter function for quantity.
         Returns the quantity (int)."""
-        return self.quantity
+        return self.__quantity
 
-    def set_quantity(self, quantity):
+    def set_quantity(self, quantity:int) -> None:
         """Setter function for quantity. If quantity reaches 0, deactivates the product."""
-        self.quantity = quantity
+        self.__quantity = quantity
         if quantity == 0:
-            self.active = True
+            self.__active = True
+
+    def get_name(self) -> str:
+        return self.__name
+
+    def set_name(self, new_name:str):
+        if len(new_name) == 0:
+            raise ValueError ("Please ensure the new name is valid")
+        self.__name = new_name
+
+    def get_price(self) -> float:
+        return self.__price
+
+    def set_price(self, new_price: float):
+        try:
+            self.__price:float = new_price
+        except ValueError as e:
+            raise ValueError(
+                "Please ensure price is a valid price"
+            ) from e
 
     def is_active(self) -> bool:
         """Getter function for active.
         Returns True if the product is active, otherwise False."""
-        return self.active
+        return self.__active
 
     def activate(self):
         """Activates the product."""
-        self.active = True
+        self.__active = True
 
     def deactivate(self):
         """Deactivates the product."""
-        self.active = False
+        self.__active = False
 
     def show(self):
         """Prints a string that represents the product, for example:
         "MacBook Air M2, Price: 1450, Quantity: 100" """
-        print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
+        return(f"{self.__name}, Price: {self.__price:.2f}, Quantity: {self.__quantity}")
 
     def buy(self, quantity) -> float:
         """Buys a given quantity of the product.
         Returns the total price (float) of the purchase.
         Updates the quantity of the product.
         In case of a problem it raises an Exception."""
-        if self.quantity < quantity:
+        if self.__quantity < quantity:
             raise ProductUnavailable(
                 "Not enough quantity available for "
-                f"product {self.name}. Only {self.quantity} items available"
+                f"product {self.__name}. Only {self.__quantity} items available"
             )
-        self.quantity -= quantity
-        return Product.calc_price(self.price, quantity)
+        if self.__active == False:
+            raise ProductUnavailable(
+                "Sorry, product can not be purchased. Product is not active")
+        self.__quantity -= quantity
+        if self.__quantity == 0:
+            self.__active = False
+        return Product.calc_price(self.__price, quantity)
 
 
 def main():
@@ -86,11 +113,11 @@ def main():
     print(mac.buy(100))
     print(mac.is_active())
 
-    bose.show()
-    mac.show()
+    print(bose.show())
+    print(mac.show())
 
     bose.set_quantity(1000)
-    bose.show()
+    print(bose.show())
 
 
 if __name__ == "__main__":
---
+++ unit_test/test_product.py
@@ -0,0 +1,114 @@
+"""
+Product testables:
+1.      Creating a new Product
+1.1     Creating a new Product with a name, price and quantity successfully
+        -   the correct show is provided
+        -   product is activated
+        -   total of available products correctly increased
+1.2     Create a new Product with an empty name
+        -   check correct assertion
+        -   check that the number of total products did not increase
+1.3     Create a new Product with a string for price
+        -   check ValueError is raised
+        -   check that no new product is created
+1.4     Create a new Product with a float for quantity
+        -   test ValueError is raised
+1.5     Create a new Product with a negative price
+        -   test ValueError is raised
+1.6     Create a new Product with a negative quantity
+2.      Deactivating a Product
+2.1     Deactivate a product by deactivate the Product
+        -   check product is deactivated
+        -   check that the product still exist with the correct price and quantity
+2.2     Deactivate a product by removing reducing the quantities to 0
+        -   check that the product is deactivated
+        -   check that the product has now quantity 0
+        -   check that the price remained the same
+2.3     Deactivate illegally.
+        Deactivate the product by directly assessing the Product variable
+        - Check that the product is still activated
+
+3.      Buying products
+3.1     Buy an active product with an available quantity
+        -   check the right price
+        -   check available quantity is reduced correctly
+        -   check that the product remains active
+
+
+"""
+import pytest
+from products import Product, ProductUnavailable
+
+# 1.1
+def test_create_new_product():
+    prod = Product("test product", 100.00, 50)
+    assert prod.is_active() == True
+    assert prod.show() == "test product, Price: 100.00, Quantity: 50"
+    assert prod.get_quantity() == 50
+
+#1.2
+def test_create_new_product_empty_name():
+    with pytest.raises(ValueError, match="Product name can not be empty"):
+        prod = Product("", 100, 50)
+
+#1.3
+def test_create_with_price_as_string():
+    with pytest.raises(ValueError, match="Please ensure price is valid"):
+        prod = Product("test product","100", 50)
+
+#1.4
+def test_create_with_quantity_as_float():
+    with pytest.raises(ValueError, match="Please ensure quantity is a valid number"):
+        prod = Product("test product",100, 50.0)
+
+# 1.5
+def test_create_product_with_negative_price():
+    with pytest.raises(ValueError, match="Please ensure price is valid"):
+        prod = Product("test product",-100, 50)
+
+
+#1.6
+def test_create_product_with_negative_quantity():
+    with pytest.raises(ValueError, match="Please ensure quantity is a valid number"):
+        prod = Product("test product",100, -50)
+
+#2.1
+def test_deactivate_by_deactivating():
+    prod = Product("Test Product", 100, 50)
+    assert prod.is_active() == True
+    prod.deactivate()
+    assert prod.is_active() == False
+    assert prod.get_price() == 100
+    assert prod.get_quantity() == 50
+
+#2.2
+def test_deactivate_by_0_quantity():
+    prod = Product("Test Product",100,50)
+    assert prod.is_active() == True
+    assert prod.get_quantity() == 50
+    price = prod.buy(50)
+    assert price == 50*100
+    assert prod.is_active() == False
+    assert prod.get_price() == 100
+    assert prod.get_quantity() == 0
+
+#2.3
+def test_illegal_deactivation():
+    prod = Product("test product", 100, 50)
+    prod.__activate = False
+    assert prod.is_active() == True
+
+def test_buy_product():
+    prod = Product("test product", 100, 50)
+    price = prod.buy(10)
+    assert price == 10*100
+    assert prod.get_quantity() == 40
+    assert prod.is_active() == True
+
+def test_buy_product_quantity_too_high():
+    prod = Product("test product", price=100.00, quantity=50)
+    with pytest.raises(ProductUnavailable, match="Not enough quantity available"):
+        price = prod.buy(51)
+
+
+
